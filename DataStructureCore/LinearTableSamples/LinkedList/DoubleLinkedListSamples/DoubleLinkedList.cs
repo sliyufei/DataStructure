1c821facab74aa11315851d533a23e8585d5e95c
liyufei <liyufeia@126.com>
2020-08-31 23:10:10 +0800

    双向链表初始化

---
+++ DataStructureCore/LinearTableSamples/LinkedList/DoubleLinkedListSamples/DoubleLinkedList.cs
@@ -0,0 +1,69 @@
+﻿using System;
+namespace DataStructureCore.LinearTableSamples.LinkedList.DoubleLinkedListSamples
+{
+    public class DoubleLinkedList<T>
+    {
+        public Node<T> First { get; set; }
+
+        public Node<T> Last { get; set; }
+
+        public int Length { get; set; }
+
+        public bool IsEmpty
+        {
+            get { return First == null; }
+        }
+
+        public void Print()
+        {
+         
+
+        }
+
+        public Node<T> GetElement(int index)
+        {
+           
+
+        }
+
+        public int GetIndex(Node<T> node)
+        {
+            
+        }
+
+
+        /// <summary>
+        /// 头插法
+        /// </summary>
+        /// <param name="node"></param>
+        public void HeadInsert(Node<T> node)
+        {
+           
+        }
+
+        /// <summary>
+        /// 尾插法
+        /// </summary>
+        /// <param name="node"></param>
+        public void TailInsert(Node<T> node)
+        {
+           
+        }
+
+
+        public void Delete(int index)
+        {
+       
+        }
+
+        public void Reverse()
+        {
+           
+        }
+
+        private Node<T> Clone(Node<T> node)
+        {
+            return new Node<T>(node.Data);
+        }
+    }
+}
---
+++ DataStructureCore/LinearTableSamples/LinkedList/DoubleLinkedListSamples/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace DataStructureCore.LinearTableSamples.LinkedList.DoubleLinkedListSamples
+{
+    public class Node<T>
+    {
+        public T Data { get; set; }
+
+        public Node<T> Previous { get; set; }
+
+        public Node<T> Next { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+        }
+    }
+}
