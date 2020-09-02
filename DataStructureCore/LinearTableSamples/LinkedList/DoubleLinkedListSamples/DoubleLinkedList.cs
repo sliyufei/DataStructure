aec5881f0fb9b02c46c69ce0927604c4f989ab8b
liyufei <liyufeia@126.com>
2020-09-02 23:35:42 +0800

    双向链表的实现

--- DataStructureCore/LinearTableSamples/LinkedList/DoubleLinkedListSamples/DoubleLinkedList.cs
+++ DataStructureCore/LinearTableSamples/LinkedList/DoubleLinkedListSamples/DoubleLinkedList.cs
@@ -16,19 +16,47 @@ namespace DataStructureCore.LinearTableSamples.LinkedList.DoubleLinkedListSample
 
         public void Print()
         {
-         
+            var currentNode = First;
+
+            while (currentNode != Last)
+            {
+                currentNode = currentNode.Next;
+            }
 
         }
 
         public Node<T> GetElement(int index)
         {
-           
+            var i = 0;
+            var currentNode = First;
+            while (currentNode != Last && i < index)
+            {
+                currentNode = currentNode.Next;
+                i++;
+            }
+
+            if (i == index)
+                return currentNode;
+            else
+                return null;
 
         }
 
         public int GetIndex(Node<T> node)
         {
-            
+            var index = 0;
+            var currentNode = First;
+            while (currentNode != null && !currentNode.Equals(node))
+            {
+                currentNode = currentNode.Next;
+                index++;
+            }
+
+            if (currentNode.Equals(node))
+                return index;
+            else
+                return -1;
+
         }
 
 
@@ -38,7 +66,22 @@ namespace DataStructureCore.LinearTableSamples.LinkedList.DoubleLinkedListSample
         /// <param name="node"></param>
         public void HeadInsert(Node<T> node)
         {
-           
+            if (IsEmpty)
+            {
+                this.First = node;
+                this.Last = node;
+
+            }
+            else
+            {
+                //新插入节点的next指向原链表的第一个节点，原链表的第一个节点的previous指向插入节点
+                node.Next = this.First;
+                this.First.Previous = node;
+                this.First = node;
+
+            }
+            this.Length++;
+
         }
 
         /// <summary>
@@ -47,23 +90,76 @@ namespace DataStructureCore.LinearTableSamples.LinkedList.DoubleLinkedListSample
         /// <param name="node"></param>
         public void TailInsert(Node<T> node)
         {
-           
+            if (IsEmpty)
+            {
+                First = node;
+                Last = node;
+
+            }
+            else
+            {
+                Last.Next = node;
+                node.Previous = this.Last;
+                Last = node;
+            }
+            this.Length++;
+
         }
+
+        public void Insert(Node<T> node,int index)
+        {
+            if (index == 0)
+            {
+                this.HeadInsert(node);
+            }
+            else if (index==this.Length-1)
+            {
+                this.TailInsert(node);
+            }
+            else
+            {
+                //建议画图，文字不好描述
+                var currentNode = this.GetElement(index);
+
+                //插入节点前趋节点指向当前节点，插入节点的后趋节点指向当前节点的下一个节点
+                node.Next = currentNode.Next;
+                node.Previous = currentNode;
+
+                //把当前节点的下一个节点的前趋节点指向插入节点
+                currentNode.Next.Previous = node;
+
+                //把当前节点的后趋节点指向插入节点
+                currentNode.Next = node;
+
+            }
+
+            this.Length++;
+
+        }
+
 
 
         public void Delete(int index)
         {
-       
-        }
 
-        public void Reverse()
-        {
-           
-        }
+            if (index==0)
+            {
+                this.First.Next.Previous = null;
+                this.First = this.First.Next;
+            }
+            else if (index == this.Length-1)
+            {
+                this.Last.Previous.Next = null;
+                this.Last = this.Last.Previous;
+            }
+            else
+            {
+                var deleteNode = this.GetElement(index);
 
-        private Node<T> Clone(Node<T> node)
-        {
-            return new Node<T>(node.Data);
+                deleteNode.Previous.Next = deleteNode.Next;
+                deleteNode.Next.Previous = deleteNode.Previous;
+            }
+
         }
     }
 }
---
+++ DataStructureTests/LinearTableTests/DoubleLinkedListTests.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using DataStructureCore.LinearTableSamples.LinkedList.DoubleLinkedListSamples;
+using Xunit;
+
+namespace DataStructureTests.LinearTableTests
+{
+    public class DoubleLinkedListTests
+    {
+        [Fact]
+        public void HeadInsert()
+        {
+            var students = new Student[]
+            {
+                new Student{ No=1,Name="李1",Score=22.1},
+                new Student{ No=2,Name="李2",Score=32.3},
+                new Student{ No=3,Name="李3",Score=42.5}
+            };
+
+            var doubleLinkedList = new DoubleLinkedList<Student>();
+
+            foreach (var student in students)
+            {
+                doubleLinkedList.HeadInsert(new Node<Student>(student));
+            }
+        }
+
+        [Fact]
+        public void TailInsert()
+        {
+            var students = new Student[]
+            {
+                new Student{ No=1,Name="李1",Score=22.1},
+                new Student{ No=2,Name="李2",Score=32.3},
+                new Student{ No=3,Name="李3",Score=42.5}
+            };
+
+            var doubleLinkedList = new DoubleLinkedList<Student>();
+
+            foreach (var student in students)
+            {
+                doubleLinkedList.TailInsert(new Node<Student>(student));
+            }
+
+        }
+
+        [Fact]
+        public void Insert()
+        {
+            var students = new Student[]
+            {
+                new Student{ No=1,Name="李1",Score=22.1},
+                new Student{ No=2,Name="李2",Score=32.3},
+                new Student{ No=3,Name="李3",Score=42.5}
+            };
+
+            var doubleLinkedList = new DoubleLinkedList<Student>();
+
+            foreach (var student in students)
+            {
+                doubleLinkedList.Insert(new Node<Student>(student),1);
+            }
+
+        }
+
+
+
+        [Fact]
+        public void Print()
+        {
+            var students = new Student[]
+            {
+                new Student{ No=1,Name="李1",Score=22.1},
+                new Student{ No=2,Name="李2",Score=32.3},
+                new Student{ No=3,Name="李3",Score=42.5}
+            };
+
+            var doubleLinkedList = new DoubleLinkedList<Student>();
+
+            foreach (var student in students)
+            {
+                doubleLinkedList.TailInsert(new Node<Student>(student));
+            }
+
+            doubleLinkedList.Print();
+        }
+
+
+        [Fact]
+        public void GetElement()
+        {
+            var students = new Student[]
+            {
+                new Student{ No=1,Name="李1",Score=22.1},
+                new Student{ No=2,Name="李2",Score=32.3},
+                new Student{ No=3,Name="李3",Score=42.5}
+            };
+
+            var doubleLinkedList = new DoubleLinkedList<Student>();
+
+            foreach (var student in students)
+            {
+                doubleLinkedList.TailInsert(new Node<Student>(student));
+            }
+
+            var node = doubleLinkedList.GetElement(2);
+        }
+
+        [Fact]
+        public void GetIndex()
+        {
+            var students = new Student[]
+            {
+                new Student{ No=1,Name="李1",Score=22.1},
+                new Student{ No=2,Name="李2",Score=32.3},
+                new Student{ No=3,Name="李3",Score=42.5}
+            };
+
+            var doubleLinkedList = new DoubleLinkedList<Student>();
+
+            foreach (var student in students)
+            {
+                doubleLinkedList.TailInsert(new Node<Student>(student));
+            }
+
+            var index = doubleLinkedList.GetIndex(doubleLinkedList.First.Next);
+        }
+
+        [Fact]
+        public void Delete()
+        {
+            var students = new Student[]
+            {
+                new Student{ No=1,Name="李1",Score=22.1},
+                new Student{ No=2,Name="李2",Score=32.3},
+                new Student{ No=3,Name="李3",Score=42.5}
+            };
+
+            var doubleLinkedList = new DoubleLinkedList<Student>();
+
+            foreach (var student in students)
+            {
+                doubleLinkedList.TailInsert(new Node<Student>(student));
+            }
+
+            doubleLinkedList.Delete(2);
+        }
+    }
+}
