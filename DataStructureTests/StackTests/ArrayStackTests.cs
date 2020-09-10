015a0d71cacb4e955f0bf6e706cf60712bd4ab9a
liyufei <liyufeia@126.com>
2020-09-10 22:37:21 +0800

    顺序栈单测

---
+++ DataStructureTests/StackTests/ArrayStackTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xunit;
+using DataStructureCore.StackSamples.ArrayStackSamples;
+using DataStructureCore.Model;
+
+namespace DataStructureTests.StackTests
+{
+    public class ArrayStackTests
+    {
+        [Fact]
+        public void PushAndPop()
+        {
+            var students = new Student[]
+            {
+                new Student{ No=1,Name="李1",Score=22.1},
+                new Student{ No=2,Name="李2",Score=32.3},
+                new Student{ No=3,Name="李3",Score=42.5}
+            };
+
+            var arrayStack = new ArrayStack<Student>(students.Length);
+
+            foreach (var student in students)
+            {
+                arrayStack.Push(student);
+            }
+
+            var popStudent = new Student();
+
+            while (arrayStack.Pop(out popStudent))
+            {
+
+            }
+
+        }
+    }
+}
