56b9ab5595b53960452f7201ba250beb37106d29
liyufei <liyufeia@126.com>
2021-01-25 21:32:30 +0800

    add idea目录忽略

--- DataStructureTests/StackTests/LinkStackTests.cs
+++ DataStructureTests/StackTests/LinkStackTests.cs
@@ -12,26 +12,23 @@ namespace DataStructureTests.StackTests
         {
             var students = new Student[]
             {
-                new Student{ No=1,Name="李1",Score=22.1},
-                new Student{ No=2,Name="李2",Score=32.3},
-                new Student{ No=3,Name="李3",Score=42.5}
+                new Student {No = 1, Name = "李1", Score = 22.1},
+                new Student {No = 2, Name = "李2", Score = 32.3},
+                new Student {No = 3, Name = "李3", Score = 42.5}
             };
 
             var linkStack = new LinkStack<Student>();
 
             foreach (var student in students)
             {
-                linkStack.Push(new Node<Student> (student));
+                linkStack.Push(new Node<Student>(student));
             }
 
             var popStudent = new Node<Student>();
 
             while (linkStack.Pop(out popStudent))
             {
-
             }
-
         }
     }
 }
-
